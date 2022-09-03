3457ca08225b443ae49dc98eac86fda88928f4b7
omkanta <1omkant1998@gmail.com>
2022-09-03 18:26:32 +0530

    mathematical operators problems

---
+++ Web22MathematicalOperator/problem2.js
@@ -0,0 +1,11 @@
+var one=1;
+var two =2;
+var three=3;
+var four=4;
+var five=5;
+one = one*2;
+two = two*2;
+three = three*2
+four = four*2;
+five = five*2;
+console.log(one+two+three+four+five);
---
+++ Web22MathematicalOperator/problem3.js
@@ -0,0 +1,12 @@
+var one=1;
+var two =2;
+var three=3;
+var four=4;
+var five=5;
+var six=6;
+two = two*2;
+three = three*3;
+four = four*3;
+five = five*2;
+six = six*2;
+console.log(one+two+three+four+five+six);
